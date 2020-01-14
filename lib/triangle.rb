e9665543ec990dd47aa7f2bb243e10d1fffd2cdd
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-14 15:32:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,10 +6,16 @@ class Triangle
     @side3 = side3
   end
 
-  # returns, as a symbol, its type. The valid types are:
-  # :equilateral, :isosceles, :scalene
+  # raise custom error (TriangleError) if the triangle is invalid.
+  # Types: :equilateral, :isosceles, :scalene
+  # Valid: The sum of the lengths of any two sides of a triangle always exceeds the length of the third side.
+  # Valid: each side must be larger than 0
   def kind
+  # if side1, side2, side3 are all equal
+      # return :equilateral
+  # if side 
 
+  # returns, as a symbol, its type. The valid types are: :equilateral, :isosceles, :scalene
     return symbol
   end
 
