20dda121128cc6af766372ae4afd568866bcd2f4
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-14 15:41:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,7 @@ class Triangle
     raise TriangleError
   elsif (@side1+@side2 <= @side3) || (@side1+@side3 <= @side2) || (@side2+@side3 <= @side1)
         raise TriangleError
-    
+
     puts ":equilateral"
 
     begin
